ed1f51b9cc56c399d4b2857a449321c276e279fc
Edward Akerboom <edward@infostreams.net>
2019-12-04 15:44:58 +0100

    Fixed 'sed' command

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ git clone https://github.com/DanielnetoDotCom/YouPHPTube-Encoder.git encoder || 
 
 # The images don't build if we use PHP 7.4 (which is what is installed
 # if we do 'FROM php:7-apache'), so we manually downgrade to PHP 7.3
-sed -i '' 's/FROM php:7-apache/FROM php:7.3-apache/g' streamer/Dockerfile
-sed -i '' 's/FROM php:7-apache/FROM php:7.3-apache/g' encoder/Dockerfile
+sed -i "s/FROM php:7-apache/FROM php:7.3-apache/g" streamer/Dockerfile
+sed -i "s/FROM php:7-apache/FROM php:7.3-apache/g" encoder/Dockerfile
 
 set -a
 source .env
