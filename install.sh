82ada333ace27c063e43b642d28a41e087b7e113
Edward Akerboom <edward@infostreams.net>
2019-12-05 14:35:18 +0100

    Try to make install fully automatic

--- install.sh
+++ install.sh
@@ -8,6 +8,35 @@ git clone https://github.com/DanielnetoDotCom/YouPHPTube-Encoder.git encoder || 
 sed -i "s/FROM php:7-apache/FROM php:7.3-apache/g" streamer/Dockerfile
 sed -i "s/FROM php:7-apache/FROM php:7.3-apache/g" encoder/Dockerfile
 
+# Adjustments to make the 'download' button work correctly
+sed -i "s/apt-get install -y/apt-get install -y libapache2-mod-xsendfile/g" streamer/Dockerfile
+sed -i "s/a2enmod rewrite/a2enmod rewrite xsendfile/g" streamer/Dockerfile
+
 set -a
 source .env
+
+docker-compose up --no-start db
+docker-compose start db # exits after starting, 'docker-compose up' does not
+
+# load databases
+cat config/streamer.sql | docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD}
+cat config/encoder.sql | docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD}
+
+# create docker volumes and copy in their respective configuration files. Make sure to use
+# the configuration values from the .env file
+docker volume create gulu_streamer_videos
+sed -i "s/MYSQL_USER/${MYSQL_USER}/g" config/streamer/configuration.php
+sed -i "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" config/streamer/configuration.php
+sed -i "s/STREAMER_HOSTNAME/${STREAMER_HOSTNAME}/g" config/streamer/configuration.php
+docker run --rm -v $PWD:/source -v gulu_streamer_videos:/dest -w /source alpine cp config/streamer/configuration.php /dest
+git checkout -- config/streamer/configuration.php
+
+docker volume create gulu_encoder_videos
+sed -i "s/MYSQL_USER/${MYSQL_USER}/g" config/encoder/configuration.php
+sed -i "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" config/encoder/configuration.php
+sed -i "s/ENCODER_HOSTNAME/${ENCODER_HOSTNAME}/g" config/encoder/configuration.php
+docker run --rm -v $PWD:/source -v gulu_encoder_videos:/dest -w /source alpine cp config/encoder/configuration.php /dest
+git checkout -- config/encoder/configuration.php
+
+# finally, start installing docker images
 docker-compose up
