107a88675b93ad165ebd3c84333e3f420e7a27e0
Edward Akerboom <edward@infostreams.net>
2019-12-05 15:38:56 +0100

    Fixes for the database installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 git clone https://github.com/DanielnetoDotCom/YouPHPTube.git streamer || true
 git clone https://github.com/DanielnetoDotCom/YouPHPTube-Encoder.git encoder || true
@@ -15,26 +15,29 @@ sed -i "s/a2enmod rewrite/a2enmod rewrite xsendfile/g" streamer/Dockerfile
 set -a
 source .env
 
+
 docker-compose up --no-start db
 docker-compose start db # exits after starting, 'docker-compose up' does not
 
 # load databases
 cat config/streamer.sql | docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD}
 cat config/encoder.sql | docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD}
+docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON video.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'"
+docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON encoder.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'"
 
 # create docker volumes and copy in their respective configuration files. Make sure to use
 # the configuration values from the .env file
 docker volume create gulu_streamer_videos
-sed -i "s/MYSQL_USER/${MYSQL_USER}/g" config/streamer/configuration.php
-sed -i "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" config/streamer/configuration.php
-sed -i "s/STREAMER_HOSTNAME/${STREAMER_HOSTNAME}/g" config/streamer/configuration.php
+sed -i s/MYSQL_USER/${MYSQL_USER}/g config/streamer/configuration.php
+sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g config/streamer/configuration.php
+sed -i s/STREAMER_HOSTNAME/${STREAMER_HOSTNAME}/g config/streamer/configuration.php
 docker run --rm -v $PWD:/source -v gulu_streamer_videos:/dest -w /source alpine cp config/streamer/configuration.php /dest
 git checkout -- config/streamer/configuration.php
 
 docker volume create gulu_encoder_videos
-sed -i "s/MYSQL_USER/${MYSQL_USER}/g" config/encoder/configuration.php
-sed -i "s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g" config/encoder/configuration.php
-sed -i "s/ENCODER_HOSTNAME/${ENCODER_HOSTNAME}/g" config/encoder/configuration.php
+sed -i s/MYSQL_USER/${MYSQL_USER}/g config/encoder/configuration.php
+sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g config/encoder/configuration.php
+sed -i s/ENCODER_HOSTNAME/${ENCODER_HOSTNAME}/g config/encoder/configuration.php
 docker run --rm -v $PWD:/source -v gulu_encoder_videos:/dest -w /source alpine cp config/encoder/configuration.php /dest
 git checkout -- config/encoder/configuration.php
 
