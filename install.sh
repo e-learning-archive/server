06f970aa7f9eccb0584a3717c960f74ac4c3ec65
Edward Akerboom <github@infostreams.net>
2019-12-11 12:30:13 +0100

    Use gsed if available, fix AVideo versions

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-echo -e "\e[32mGetting YouPHPTube source code\e[39m"
-git clone https://github.com/DanielnetoDotCom/YouPHPTube.git streamer || true
-git clone https://github.com/DanielnetoDotCom/YouPHPTube-Encoder.git encoder || true
+
+# we checkout a 'known good version' of the required software
+echo -e "\e[32mGetting AVideo source code\e[39m"
+git clone https://github.com/WWBN/AVideo.git streamer || true
+cd streamer && git reset --hard 2dae06ebdb9f3c912e86b5a96baf0a32f3d59972 && cd ..
+git clone https://github.com/WWBN/AVideo-Encoder.git encoder || true
+cd streamer && git reset --hard b5cca3ef906ce9659f3249b7319374e2c74d8217 && cd ..
+
+# if the 'gsed' command exists, use that - otherwise, default to 'sed'
+if [ -x "$(command -v gsed)" ]; then
+  SED=`which gsed`
+else
+  SED=`which sed`
+fi
 
 # The images don't build if we use PHP 7.4 (which is what is installed
 # if we do 'FROM php:7-apache'), so we manually downgrade to PHP 7.3
-sed -i "s/FROM php:7-apache/FROM php:7.3-apache/g" streamer/Dockerfile
-sed -i "s/FROM php:7-apache/FROM php:7.3-apache/g" encoder/Dockerfile
+$SED -i "s/FROM php:7-apache/FROM php:7.3-apache/g" streamer/Dockerfile
+$SED -i "s/FROM php:7-apache/FROM php:7.3-apache/g" encoder/Dockerfile
 
 # Adjustments to make the 'download' button work correctly
-sed -i "s/apt-get install -y/apt-get install -y libapache2-mod-xsendfile/g" streamer/Dockerfile
-sed -i "s/a2enmod rewrite/a2enmod rewrite xsendfile/g" streamer/Dockerfile
+$SED -i "s/apt-get install -y/apt-get install -y libapache2-mod-xsendfile/g" streamer/Dockerfile
+$SED -i "s/a2enmod rewrite/a2enmod rewrite xsendfile/g" streamer/Dockerfile
 
 set -a
 source .env
@@ -32,17 +43,17 @@ docker exec -i `docker-compose ps -q db` /usr/bin/mysql -u root --password=${MYS
 # the configuration values from the .env file
 echo -e "\e[32mBuilding ${STREAMER_HOSTNAME} streaming site\e[39m"
 docker-compose up --no-start streamer
-sed -i s/MYSQL_USER/${MYSQL_USER}/g config/streamer/configuration.php
-sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g config/streamer/configuration.php
-sed -i s/STREAMER_HOSTNAME/${STREAMER_HOSTNAME}/g config/streamer/configuration.php
+$SED -i s/MYSQL_USER/${MYSQL_USER}/g config/streamer/configuration.php
+$SED -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g config/streamer/configuration.php
+$SED -i s/STREAMER_HOSTNAME/${STREAMER_HOSTNAME}/g config/streamer/configuration.php
 docker run --rm -v $PWD:/source -v gulu_streamer_videos:/dest -w /source alpine cp config/streamer/configuration.php /dest
 git checkout -- config/streamer/configuration.php
 
 echo -e "\e[32mBuilding ${ENCODER_HOSTNAME} video encoder site\e[39m"
 docker-compose up --no-start encoder
-sed -i s/MYSQL_USER/${MYSQL_USER}/g config/encoder/configuration.php
-sed -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g config/encoder/configuration.php
-sed -i s/ENCODER_HOSTNAME/${ENCODER_HOSTNAME}/g config/encoder/configuration.php
+$SED -i s/MYSQL_USER/${MYSQL_USER}/g config/encoder/configuration.php
+$SED -i s/MYSQL_PASSWORD/${MYSQL_PASSWORD}/g config/encoder/configuration.php
+$SED -i s/ENCODER_HOSTNAME/${ENCODER_HOSTNAME}/g config/encoder/configuration.php
 docker run --rm -v $PWD:/source -v gulu_encoder_videos:/dest -w /source alpine cp config/encoder/configuration.php /dest
 git checkout -- config/encoder/configuration.php
 
