0b457a76e66743a23ea85e3668120720552ca506
Edward Akerboom <github@infostreams.net>
2020-01-09 16:26:45 +0100

    Checkout correct branch

--- install.sh
+++ install.sh
@@ -47,10 +47,18 @@ download() {
   repo=$1
   dir=$2
   desc=$3
+  branch=$4
   echo -e "\033[32m- ${desc}\033[39m"
 
   if ! [ -d "$dir" ]; then
     git clone "$repo" "$dir" || true
+
+    if ! [ -z "$branch" ]; then
+      pwd=$(pwd)
+      cd "$dir"
+      git checkout -t "$branch" || true
+      cd "$pwd"
+    fi
   else
     echo -e "  ⤏ fetching latest version"
     pwd=$(pwd)
@@ -62,7 +70,7 @@ download() {
 
 echo -e '\033[34mGetting all source code\033[39m'
 download https://github.com/e-learning-archive/AVideo.git src/streamer "video streamer"
-download https://github.com/e-learning-archive/AVideo-Encoder.git src/encoder "video encoder"
+download https://github.com/e-learning-archive/AVideo-Encoder.git src/encoder "video encoder" "origin/e-learning-archive"
 download https://github.com/e-learning-archive/coursera-dl.git src/coursera-dl "coursera downloader"
 download https://github.com/e-learning-archive/edx-dl.git src/edx-dl "edX downloader"
 
